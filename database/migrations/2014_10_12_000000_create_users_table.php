f93f830a771554c5c7648409ce7e266004f305e0
178inaba <178inaba@users.noreply.github.com>
2016-04-04 13:31:52 +0900

    fix password column length

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('name', 255);
             $table->string('email', 255);
             $table->tinyInteger('authority');
-            $table->string('password');
+            $table->string('password', 255);
             $table->rememberToken();
             $table->timestamps();
 
