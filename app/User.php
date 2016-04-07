a562a343eb30f0286932a8e8f402ec7761881d29
178inaba <178inaba@users.noreply.github.com>
2016-04-07 11:11:38 +0900

    fix model

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'company_id', 'system_name', 'name', 'email', 'authority', 'password',
     ];
 
     /**
